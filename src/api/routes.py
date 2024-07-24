e61c76cf7c01bfb9d8a5a4f280a27dddafbe1983
Carmen Morcuende <166156217+carmenmorc@users.noreply.github.com>
2024-07-24 19:47:02 +0000

    Backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -23,7 +23,51 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@api.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    #user = User.query.filter_by(email=email, password=password).first()
+    user = User.query.filter_by(email=email).first()
+    if user:
+        if (user.password == password):
+            access_token = create_access_token(identity=user.id)
+            return jsonify({'success' : True, 'user':user.serialize(), 'token' : access_token}), 200
+        return jsonify({'success' : False,  'msg' : 'Combinación de usuario y contraseña no válida'}), 400
+    return jsonify({'success' : False,  'msg' : 'El correo electrónico no tiene una cuenta asociada'}), 404
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    if not email or not password:
+        return jsonify({'success' : False,  'msg' : 'Todos los campos son necesarios'}), 400
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({'success' : False,  'msg' : 'Este correo electrónico ya tiene una cuenta'}), 400
+    new_user = User(email=email, password=password, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    access_token = create_access_token(identity=new_user.id)
+    return jsonify({'success' : True, 'user': new_user.serialize(), 'token' : access_token}), 200
+
+
+@api.route('/token', methods=['GET'])
+@jwt_required()
+def check_jwt():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({'success' : True, 'user': user.serialize()}), 200
+    return jsonify({'success' : False, 'msg': 'Bad Token'}), 200
+
 @api.route('/protected', methods=['GET'])
-@jwt_required
+@jwt_required()
 def handle_protected():
-    return jsonify({'msg' : 'Has logrado acceder a una ruta protegida'})
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    if user:
+         return jsonify({'msg' : 'Has logrado acceder a una ruta protegida, ' + user})
+    return jsonify({'Success' : False, 'msg' : 'Bad Token'})
+
+   
